b85a8140953ef4962c584b16c45a116ee1549af6
how melnyczuk <how@melnycz.uk>
2021-04-20 17:56:09 +0300

    tidy

--- src/client/renderer.js
+++ src/client/renderer.js
@@ -4,40 +4,53 @@ const BASE_URL = 'ws://127.0.0.1:4242';
 const SUBTITLE_CHANGE_RATE = 10 * 1000;
 const SUBTITLES_QUERY = '#subtitles';
 
+const streamContraints = {
+  video: {
+    width: { max: 3840, ideal: 3840, min: 1920 },
+    height: { max: 2160, ideal: 2160, min: 1080 },
+    frameRate: { max: 30, ideal: 30, min: 25 },
+    zoom: 100,
+  },
+  audio: false,
+};
+
 function sanitizeDataUrl(url) {
   return url.replace('data:', '').replace(/^.+,/, '');
 }
 
-function getSendFrame(imgCap, socket) {
+function getSendFrame(imageCapture, socket) {
   const reader = new FileReader();
 
   reader.onload = () => {
-    console.log('sending image...');
     socket.send(sanitizeDataUrl(reader.result));
   };
 
-  return () => {
-    imgCap.takePhoto().then((blob) => {
-      reader.readAsDataURL(blob);
-    });
-  };
+  return () =>
+    imageCapture
+      .takePhoto()
+      .then((blob) => {
+        reader.readAsDataURL(blob);
+      })
+      .catch(console.error);
+}
+
+function setSubtitleContent(subtitle) {
+  document.querySelector(SUBTITLES_QUERY).textContent = subtitle;
 }
 
 function getMessageHandler(reconnect) {
   const update = (subtitles) => {
     const [sub, ...rest] = subtitles;
-    document.querySelector(SUBTITLES_QUERY).textContent = sub;
-    console.log('rest length', rest.length);
+    setSubtitleContent(sub);
     setTimeout(
-      () => (rest.length === 0 ? reconnect() : update(rest)),
+      () => (rest.length > 0 ? update(rest) : reconnect()),
       SUBTITLE_CHANGE_RATE
     );
   };
 
   return (data) => {
     const { text } = JSON.parse(data);
-    console.log('Adding subtitles: ', text.length);
-    update(text);
+    update([...text, '']);
   };
 }
 
@@ -50,23 +63,25 @@ function connect(imgCap) {
 
   socket.onmessage = ({ data }) => handler(data);
   socket.onclose = () => console.log('disconnected');
+  socket.onerror = (e) => console.error(e);
   socket.onopen = () => sendFrame();
 }
 
+function handleStream(stream) {
+  const videoTrack = stream.getVideoTracks()[0];
+  connect(new ImageCapture(videoTrack));
+  document.getElementById('webcam').srcObject = stream;
+}
+
+const lenSocket = new WebSocket(`${BASE_URL}/len`);
+lenSocket.onopen = () => lenSocket.send(80);
+
 const pingSocket = new WebSocket(`${BASE_URL}/ping`);
 pingSocket.onopen = () => pingSocket.send('ping...');
-pingSocket.onclose = () => console.log('Ping socket closed');
+pingSocket.onclose = () => console.log('');
 pingSocket.onmessage = () => {
-  navigator.getUserMedia(
-    { video: true, audio: false },
-    (stream) => {
-      const [videoTrack] = stream.getVideoTracks();
-      const imgCap = new ImageCapture(videoTrack);
-
-      connect(imgCap);
-
-      document.getElementById('webcam').srcObject = stream;
-    },
-    console.error
-  );
+  navigator.mediaDevices
+    .getUserMedia(streamContraints)
+    .then(handleStream)
+    .catch(console.error);
 };
